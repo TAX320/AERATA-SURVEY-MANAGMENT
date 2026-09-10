ee9002f4abe739c35b12df7d809d362bf6eba4ad
TAX320 <mgforos1220@gmail.com>
2026-09-10 15:04:11 +0300

    Add dynamic client list with sector filter

--- survey/urls.py
+++ survey/urls.py
@@ -11,3 +11,11 @@ urlpatterns = [
     path('projects/', views.project_list, name='project_list'),
     path('projects/<int:pk>/', views.project_detail, name='project_detail'),
 ]
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('projects/', views.project_list, name='project_list'),
+    path('projects/<int:pk>/', views.project_detail, name='project_detail'),
+    path('clients/', views.client_list, name='client_list'),
+]
--- survey/views.py
+++ survey/views.py
@@ -16,3 +16,32 @@ def project_detail(request, pk):
         'project': project,
         'deliverables': deliverables,
     })
+    from django.shortcuts import render, get_object_or_404
+from .models import SurveyProject, Client
+
+
+def project_list(request):
+    projects = SurveyProject.objects.select_related('client', 'service_type').all()
+    return render(request, 'survey/project_list_dynamic.html', {'projects': projects})
+
+
+def project_detail(request, pk):
+    project = get_object_or_404(
+        SurveyProject.objects.select_related('client', 'service_type'), pk=pk
+    )
+    deliverables = project.deliverables.all()
+    return render(request, 'survey/project_detail_dynamic.html', {
+        'project': project,
+        'deliverables': deliverables,
+    })
+
+
+def client_list(request):
+    clients = Client.objects.all()
+    sector = request.GET.get('sector')
+    if sector:
+        clients = clients.filter(sector=sector)
+    return render(request, 'survey/client_list_dynamic.html', {
+        'clients': clients,
+        'selected_sector': sector,
+    })
