2135561393290b81adbf4523699f26216b670f41
TAX320 <mgforos1220@gmail.com>
2026-09-10 14:59:13 +0300

    Add dynamic project detail view

--- survey/urls.py
+++ survey/urls.py
@@ -4,3 +4,10 @@ from . import views
 urlpatterns = [
     path('projects/', views.project_list, name='project_list'),
 ]
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('projects/', views.project_list, name='project_list'),
+    path('projects/<int:pk>/', views.project_detail, name='project_detail'),
+]
--- survey/views.py
+++ survey/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import SurveyProject
 
 
 def project_list(request):
     projects = SurveyProject.objects.select_related('client', 'service_type').all()
     return render(request, 'survey/project_list_dynamic.html', {'projects': projects})
+
+
+def project_detail(request, pk):
+    project = get_object_or_404(
+        SurveyProject.objects.select_related('client', 'service_type'), pk=pk
+    )
+    deliverables = project.deliverables.all()
+    return render(request, 'survey/project_detail_dynamic.html', {
+        'project': project,
+        'deliverables': deliverables,
+    })
